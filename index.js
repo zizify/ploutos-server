fba85bdff89781b1b8f13806683c755d491aa22c
Andy Gaines <andyagaines@gmail.com>
2018-02-28 16:59:00 -0600

    Sets up database and removes extraneous files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+require('dotenv').config();
 
 const {PORT, CLIENT_ORIGIN} = require('./config');
 const {dbConnect} = require('./db-mongoose');
@@ -9,31 +10,31 @@ const {dbConnect} = require('./db-mongoose');
 const app = express();
 
 app.use(
-    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-        skip: (req, res) => process.env.NODE_ENV === 'test'
-    })
+	morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+		skip: (req, res) => process.env.NODE_ENV === 'test'
+	})
 );
 
 app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
+	cors({
+		origin: CLIENT_ORIGIN
+	})
 );
 
 function runServer(port = PORT) {
-    const server = app
-        .listen(port, () => {
-            console.info(`App listening on port ${server.address().port}`);
-        })
-        .on('error', err => {
-            console.error('Express failed to start');
-            console.error(err);
-        });
+	const server = app
+		.listen(port, () => {
+			console.info(`App listening on port ${server.address().port}`);
+		})
+		.on('error', err => {
+			console.error('Express failed to start');
+			console.error(err);
+		});
 }
 
 if (require.main === module) {
-    dbConnect();
-    runServer();
+	dbConnect();
+	runServer();
 }
 
 module.exports = {app};
